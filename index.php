0e515484b0e8c20b081fa881618018f8740f8bfe
jzdev <jzdev@outlook.com>
2014-10-03 13:02:28 -0400

    moved require for logic.php

--- index.php
+++ index.php
@@ -1,56 +1,16 @@
-<?php require("/includes/logic.php"); ?>
+warning: templates not found C:\Program Files\Cmder\vendor\msysgit
+/share/git-core/templates
+Initialized empty Git repository in C:/xampp/htdocs/p2/.git/
 
-<!DOCTYPE html>
-<html>
-	<?php include("/includes/header.php"); ?>
-	<body>
-		<div id="maincontent">
-			<h1>Welcome to the xkcd Password Generator!</h1>
-			
-			<?php if (isset($msg)): ?>
-					<p class="error"><?php echo $msg ?></p>
-			<?php endif ?>			
 
-			<?php if (isset($password)): ?>
-					<h2 class="pswdmsg">Your password is: <span class="password"><?=$password?></span></h2>
-			<?php endif ?>			
 
-			<div id="leftcolumn">
-				<h2>What is it?</h2>
-				<p>The xkcd password generator will create a password that is secure 
-					and easy to remember! The idea came from a webcomic created by Randall Munroe 
-					(see below) in which he believes a multi-word password is more secure
-					than the traditional cryptic passwords AND of course, easier to remember!
-				</p>
-				<h2>How does it work?</h2>
-				<p>A password will be generated from a random list of common words.  For additional
-					security, you have the option to append the last word of your password with either
-					a symbol and\or number and choose to capitalize the first letter of your password.
-				</p>
-			</div>
+Permission denied when trying to push to github
 
-			<div id="rightcolumn">
-				<h2>Generate a new password</h2>
-				<form action="index.php" method="POST">
-				
-					<label for="numwords">Number of words: </label>
-					<input type="text" name="numwords" id="numwords" maxlength="1" value="3" /><br>
 
-					<label for="uppercase">Uppercase first letter?: </label>
-					<input type="checkbox" name="uppercase" id="uppercase" value="uppercase" /><br>
+C:\xampp\htdocs\p2 (master)
+λ git push github master
+Permission denied (publickey).
+fatal: Could not read from remote repository.
 
-					<label for="symbol">Include a symbol?: </label>
-					<input type="checkbox" name="symbol" id="symbol" value="symbol" /><br>
-
-					<label for="number">Include a number?: </label>
-					<input type="checkbox" name="number" id="number" value="number" /><br>
-
-					<input type="submit" id="submit" name="submit" value="Get Your Password!">
-				</form>	
-			</div>
-			<div id="comic">
-				<img src="img/xkcd.png" alt="xkcd Password Generator Comic">
-			</div>	
-		</div>
-	</body>
-</html>	
+Please make sure you have the correct access rights
+and the repository exists.
